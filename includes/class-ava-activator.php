d892c7b1e9a6ea8318b7cdb8ccbc87890f99a8c3
Nicolas Munoz Alvarez <nicom1998.5@gmail.com>
2023-04-11 07:20:38 -0500

    Ajustes de indicadores iniciales

--- includes/class-ava-activator.php
+++ includes/class-ava-activator.php
@@ -39,11 +39,25 @@ class AVA_Activator
 
 		$sql_insert = "INSERT INTO " . DASHBOARD_AVA_TABLE . " (key_kpi, name_kpi, comment_kpi) VALUES (";
 
-		$sql_3 = $sql_insert . "'fin_rentabilidad', 'Rentabilidad', 'Prueba del indicador de rentabilidad');";
-		$sql_4 = $sql_insert . "'fin_rotacion', 'Rotación', 'Prueba del indicador de rotación.');";
+		$sql_3 = $sql_insert . "'fin_liquidez', 'Razón Corriente (Liquidez)', 'Razón corriente = Activo corriente / Pasivo corriente. Periodicidad Mensual');";
+		$sql_4 = $sql_insert . "'fin_endeudamiento', 'Nivel de Endeudamento', 'Porcentaje total de la deuda que tiene un negocio con relación a sus recursos propios. Periodicidad Mensual');";
+		$sql_5 = $sql_insert . "'fin_utilidad', 'Utilidad Neta', 'Utilidad bruta – impuestos – intereses – depreciación – gastos generales. Periodicidad Mensual');";
+
+		$sql_6 = $sql_insert . "'cli_satisfaccion', 'Nivel de satisfacción de clientes', 'Promedio de resultados de encuesta de satisfacción. Periodicidad Anual');";
+		$sql_7 = $sql_insert . "'cli_facturacion', 'Incremento de la facturación anual', '(Facturación periodo año actual/Facturación periodo año anterior) - 1 * 100. Periodicidad Anual');";
+
+		$sql_8 = $sql_insert . "'proc_impl_gestion', 'Implementación Sis. de Gestión', '% de Cumplimiento Plan de  Implementación Sistema de Gestión. No de Actividades ejecutadas/Número de actividades programadas *100. Periodicidad Anual');";
+		$sql_9 = $sql_insert . "'proc_impl_software', 'Implementación de Software', '% de Cumplimiento Plan de  Implementación Software. No de Actividades ejecutadas/Número de actividades programadas *100. Periodicidad Anual');";
 
 		$wpdb->query($sql_3);
 		$wpdb->query($sql_4);
+		$wpdb->query($sql_5);
+
+		$wpdb->query($sql_6);
+		$wpdb->query($sql_7);
+
+		$wpdb->query($sql_8);
+		$wpdb->query($sql_9);
 
 	}
 
